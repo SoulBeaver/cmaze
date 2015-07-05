101b09256a9db0e653e394a5f5087bdedbff8a4c
Christian Broomfield <christian.broomfield@posteo.de>
2015-07-05 14:41:48 +0200

    Mod: Changed the way a maze is generated within a maze.

--- src/cmaze/maze.clj
+++ src/cmaze/maze.clj
@@ -1,9 +1,9 @@
 (ns cmaze.maze
   (use cmaze.cell))
 
-(defrecord Maze [cells])
+(defrecord Maze [cells size])
 
-(defn create-maze-cell [index size]
+(defn- create-maze-cell [index size]
   "Creates a cell depending on its location in the maze."
   (let [is-only-cell (= size 1)
         [is-corner-cell corner] (m-corner-cell? index size)
@@ -18,4 +18,47 @@
   (Maze.
     (let [total-cells (* size size)]
       (map-indexed (fn [idx _] (create-maze-cell idx size))
-                   (range total-cells)))))
+        (range total-cells)))
+    size))
+
+(defn get-cell [maze x y]
+  (let [cells (:cells maze)
+        size (:size maze)
+        index (+ x (* y size))]
+    (when (> index (* size size)) (throw (IllegalArgumentException. (format "Index (%s, %s) exceeds size of maze." x y))))
+    (nth cells index)))
+
+(defn- link [cell dir]
+  (assoc cell :open-dirs dir))
+
+(defn- link-cell [maze cell idx]
+  (let [{cells :cells size :size} maze
+        {open-dirs :open-dirs} cell]
+    (doseq [open-dirs open-dirs]
+      (fn [open-dir]
+        (case open-dir
+          :east  (link (nth cells (inc idx)) :west)
+          :west  (link (nth cells (dec idx)) :east)
+          :north (link (nth cells (- idx size)) :south)
+          :south (link (nth cells (+ idx size)) :north))))
+    cell))
+
+(defn link-cells [maze cells]
+  (for [cell cells
+        idx (range (count cells))]
+    (link-cell maze cell idx)))
+
+(defn traverse [maze f]
+  (let [{cells :cells size :size} maze]
+      (map (fn [cell] (f cell))
+        cells)))
+
+(defn binary-tree [cell]
+  (let [valid-dirs (:valid-dirs cell)]
+    (cond
+      (and (contains? valid-dirs :north) (contains? valid-dirs :east)) (assoc cell :open-dirs #{:east})
+      (contains? valid-dirs :north) (assoc cell :open-dirs #{:north})
+      (contains? valid-dirs :east) (assoc cell :open-dirs #{:east})
+      :else cell)))
+
+;; sample usage: (link-cells maze (traverse maze binary-tree))
